7de509ef2470ea7abe8d8a9025a3737205d4c8f9
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2021-08-18 21:18:07 +0300

    Revert "init/userland_worker: Fix permissions for proximity sensor"
    This reverts commit d9b46f6d75f963991f1b1d6292d8084a58eadce7.

--- init/userland_worker.c
+++ init/userland_worker.c
@@ -115,10 +115,8 @@ static void vbswap_help(void)
 	linux_sh("/system/bin/swapon /dev/block/vbswap0");
 }
 
-static void fix_proximity(void) {
-	linux_sh("/system/bin/chmod 666 /dev/input/event4");
-	linux_sh("/system/bin/chmod 666 /dev/input/event9");
-	linux_sh("/system/bin/chmod 666 /dev/input/event11");
+static void restart_proximity(void) {
+	//TODO
 }
 
 static void userland_worker(struct work_struct *work)
@@ -141,7 +139,7 @@ static void userland_worker(struct work_struct *work)
 
 	vbswap_help();
 
-	fix_proximity();
+	//restart_proximity();
 
 	if (is_enforcing) {
 		pr_info("Going enforcing");
